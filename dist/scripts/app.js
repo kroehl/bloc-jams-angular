c792f3bd5d18e8819801dbe808809a9aa480786c
Kristin <kroehl13@gmail.com>
2016-07-06 19:27:56 -0400

    Assignment 5: album ctrl

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -16,6 +16,7 @@ angular.module('blocJams', []);
         })
         .state('album', {
             url: '/album',
+            controller: 'AlbumCtrl as album',
             templateUrl: '/templates/album.html'
         })
         .state('collection', {
